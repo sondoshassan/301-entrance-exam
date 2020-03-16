bb61bc089cd13ebb38d45cc6c1fe6abf0bdfede3
sondoshassan <sondos199628@gmail.com>
2020-03-16 12:26:55 +0200

    solve q1

---
+++ js/q1.js
@@ -0,0 +1,15 @@
+'use strict';
+var counter = 0;
+
+var result = greaterThan([2,3,4,1,10,0,6],2);
+console.log(result);
+
+
+function greaterThan(testArr,testNum) {
+  for (var i = 0; i < testArr.length; i++) {
+    if (testArr[i] > testNum) {
+      counter++;
+    }
+  }
+  return counter;
+}
